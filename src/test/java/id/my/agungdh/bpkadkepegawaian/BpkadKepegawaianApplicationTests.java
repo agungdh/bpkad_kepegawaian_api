b91b926d67fa311ce37be42d87dba101bc546f96
AgungDH <agungdh@live.com>
2026-02-05 10:28:02 +0700

    init

---
+++ src/test/java/id/my/agungdh/bpkadkepegawaian/BpkadKepegawaianApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.agungdh.bpkadkepegawaian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BpkadKepegawaianApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
